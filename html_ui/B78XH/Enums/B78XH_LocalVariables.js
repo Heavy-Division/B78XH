cdb159b0dc7eceae0358ce1a61bd79bf7234e468
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-02-17 09:39:31 +0100

    LocalVars changed fo B78XH

---
+++ html_ui/B78XH/Enums/B78XH_LocalVariables.js
@@ -0,0 +1,39 @@
+const B78XH_LocalVariables = {
+	VERSION: 'B78XHL',
+	IRS: {
+		L: {
+			STATE: 'L:' + this.VERSION + '_IRS_L_STATE',
+			SWITCH_STATE: 'L:' + this.VERSION + '_IRS_L_SWITCH_STATE',
+			INIT_TIME:  'L:' + this.VERSION + '_IRS_L_INIT_TIME',
+			TIME_FOR_ALIGN: 'L:' + this.VERSION + '_IRS_L_TIME_FOR_ALIGN'
+		},
+		C: {
+			STATE: 'L:' + this.VERSION + '_IRS_C_STATE',
+			SWITCH_STATE: 'L:' + this.VERSION + '_IRS_C_SWITCH_STATE',
+			INIT_TIME:  'L:' + this.VERSION + '_IRS_C_INIT_TIME',
+			TIME_FOR_ALIGN: 'L:' + this.VERSION + '_IRS_C_TIME_FOR_ALIGN'
+		},
+		R: {
+			STATE: 'L:' + this.VERSION + '_IRS_R_STATE',
+			SWITCH_STATE: 'L:' + this.VERSION + '_IRS_R_SWITCH_STATE',
+			INIT_TIME:  'L:' + this.VERSION + '_IRS_R_INIT_TIME',
+			TIME_FOR_ALIGN: 'L:' + this.VERSION + '_IRS_R_TIME_FOR_ALIGN'
+		},
+		IS_INITED: 'L:' + this.VERSION + '_IRS_IS_INITED',
+		POSITION_SET: 'L:' + this.VERSION + '_IRS_POSITION_SET'
+	},
+	APU: {
+		EGT: 'L:' + this.VERSION + '_APU_EGT',
+		OIL_PRESS: 'L:' + this.VERSION + '_APU_OIL_PRESS',
+		OIL_TEMP: 'L:' + this.VERSION + '_APU_OIL_TEMP',
+		RPM: 'APU PCT RPM',
+		SWITCH_STATE: 'A:APU SWITCH'
+	},
+	VNAV: {
+		CUSTOM_VNAV_CLIMB_ENABLED: 'L:' + this.VERSION + '_VNAV_CUSTOM_CLIMB_ENABLED'
+	},
+
+	SIM_RATE_MANAGER: {
+		ACTIVATED: 'L:' + this.VERSION + '_SIM_RATE_MANAGER_ACTIVATED'
+	}
+}
