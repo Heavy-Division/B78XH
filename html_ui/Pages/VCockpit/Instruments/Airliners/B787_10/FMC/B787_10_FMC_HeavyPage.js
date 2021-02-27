369b3524bde0f4044f71ebd1f986ec064d017777
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-02-27 17:56:08 +0100

    possibility to rid of managers

--- html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC_HeavyPage.js
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC_HeavyPage.js
@@ -1,27 +1,47 @@
+Include.addScript('/Heavy/Utils/HeavyDataStorage.js');
+
 class B787_10_FMC_HeavyPage {
 	static ShowPage1(fmc) {
 		fmc.clearDisplay();
-		fmc.setTemplate([
+
+		let rows = [
 			['HEAVY MENU'],
 			[''],
-			['', '<Payload Manager'],
+			['', ''],
 			[''],
-			['', '<SimRate Manager'],
-			[''],
-			[''],
+			['', ''],
 			[''],
 			[''],
 			[''],
 			[''],
 			[''],
-			['']
-		]);
-		fmc.onRightInput[0] = () => {
-			new B787_10_FMC_PayloadManagerPage(fmc).showPage()
+			[''],
+			[''],
+			['', '<CONFIGURATION']
+		];
+
+		console.log(HeavyDataStorage.get('DISABLE_MANAGERS_FOREVER'))
+
+		if (HeavyDataStorage.get('DISABLE_MANAGERS_FOREVER', 'false') == 'false') {
+
+			rows[2] = ['', '<Payload Manager'];
+			rows[4] = ['', '<SimRate Manager'];
+
+			fmc.onRightInput[0] = () => {
+				new B787_10_FMC_PayloadManagerPage(fmc).showPage();
+			};
+			fmc.onRightInput[1] = () => {
+				new B787_10_FMC_SimRateManagerPage(fmc).showPage();
+			};
+		}
+
+		fmc.onRightInput[5] = () => {
+			B787_10_FMC_HeavyConfigurationPage.ShowPage1(fmc);
 		};
-		fmc.onRightInput[1] = () => {
-			new B787_10_FMC_SimRateManagerPage(fmc).showPage();
-		};
+
+		fmc.setTemplate(
+			rows
+		);
 
 		fmc.updateSideButtonActiveStatus();
 	}
