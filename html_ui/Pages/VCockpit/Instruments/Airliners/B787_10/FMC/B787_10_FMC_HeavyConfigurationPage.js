c21c09509fe37bbb3bd246255178f575ec822de3
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-05-11 15:27:22 +0200

    FPSYNC ADDED HEAVY CONFIGURATION Page changed

--- html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC_HeavyConfigurationPage.js
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC_HeavyConfigurationPage.js
@@ -3,14 +3,17 @@ Include.addScript('/Heavy/Utils/HeavyDataStorage.js');
 class B787_10_FMC_HeavyConfigurationPage {
 	static ShowPage1(fmc) {
 		fmc.clearDisplay();
+
+		let fpSyncCell = (this.isFPSyncActive() ? '<[color=green]ON[/color]←→[size=small]OFF[/size]' : '<[size=small]ON[/size]←→[color=red]OFF[/color]')
+		let simBriefCell = (this.isSimBriefFilled() ? '<[color=green]FILLED[/color]' : '<[color=red]NOT FILLED[/color]')
 		fmc.setTemplate([
 			['HEAVY CONFIGURATION'],
-			[''],
-			['', '<SimBrief'],
-			[''],
-			[''],
+			['', 'SimBrief'],
+			['', simBriefCell],
 			[''],
 			[''],
+			['', 'FP SYNC'],
+			['', fpSyncCell],
 			[''],
 			[''],
 			[''],
@@ -32,5 +35,25 @@ class B787_10_FMC_HeavyConfigurationPage {
 		fmc.onRightInput[0] = () => {
 			new B787_10_FMC_SimBriefConfigurationPage(fmc).showPage();
 		}
+
+		fmc.onRightInput[2] = () => {
+			if(this.isFPSyncActive()){
+				WTDataStore.set('WT_CJ4_FPSYNC', 0)
+			} else {
+				WTDataStore.set('WT_CJ4_FPSYNC', 1)
+			}
+			B787_10_FMC_HeavyConfigurationPage.ShowPage1(fmc);
+		}
+	}
+
+	static isSimBriefFilled(){
+		let username = HeavyDataStorage.get('SIMBRIEF_USERNAME', '');
+		let userid = HeavyDataStorage.get('SIMBRIEF_USERID', '');
+
+		return (username !== '' || userid !== '');
+	}
+
+	static isFPSyncActive(){
+		return (!!WTDataStore.get('WT_CJ4_FPSYNC', 0));
 	}
 }
