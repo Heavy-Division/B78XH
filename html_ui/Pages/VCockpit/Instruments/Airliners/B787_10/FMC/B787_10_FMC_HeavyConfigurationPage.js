3e814de76ba879fedbf266c0b91739e38a55253c
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-05-11 18:21:13 +0200

    FMC - It is possible to switch between IMPERIAL/METRIC units FMC - InitPerf page support IMPERIAL/METRIC units

--- html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC_HeavyConfigurationPage.js
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC_HeavyConfigurationPage.js
@@ -4,16 +4,17 @@ class B787_10_FMC_HeavyConfigurationPage {
 	static ShowPage1(fmc) {
 		fmc.clearDisplay();
 
-		let fpSyncCell = (this.isFPSyncActive() ? '<[color=green]ON[/color]←→[size=small]OFF[/size]' : '<[size=small]ON[/size]←→[color=red]OFF[/color]')
-		let simBriefCell = (this.isSimBriefFilled() ? '<[color=green]FILLED[/color]' : '<[color=red]NOT FILLED[/color]')
+		let fpSyncCell = (this.isFPSyncActive() ? '<[color=green]ON[/color]←→[size=small]OFF[/size]' : '<[size=small]ON[/size]←→[color=red]OFF[/color]');
+		let simBriefCell = (this.isSimBriefFilled() ? '<[color=green]FILLED[/color]' : '<[color=red]NOT FILLED[/color]');
+		let unitsCell = (this.useImperial() ? '<[color=green]IMPERIAL[/color]←→[size=small]METRIC[/size]' : '<[size=small]IMPERIAL[/size]←→[color=green]METRIC[/color]');
 		fmc.setTemplate([
 			['HEAVY CONFIGURATION'],
 			['', 'SimBrief'],
 			['', simBriefCell],
-			[''],
-			[''],
 			['', 'FP SYNC'],
 			['', fpSyncCell],
+			['', 'UNITS'],
+			['', unitsCell],
 			[''],
 			[''],
 			[''],
@@ -27,33 +28,50 @@ class B787_10_FMC_HeavyConfigurationPage {
 		fmc.updateSideButtonActiveStatus();
 	}
 
-	static setupInputHandlers(fmc){
+	static setupInputHandlers(fmc) {
 		fmc.onLeftInput[5] = () => {
 			B787_10_FMC_HeavyPage.ShowPage1(fmc);
-		}
+		};
 
 		fmc.onRightInput[0] = () => {
 			new B787_10_FMC_SimBriefConfigurationPage(fmc).showPage();
-		}
+		};
 
-		fmc.onRightInput[2] = () => {
-			if(this.isFPSyncActive()){
-				WTDataStore.set('WT_CJ4_FPSYNC', 0)
+		fmc.onRightInput[1] = () => {
+			if (this.isFPSyncActive()) {
+				WTDataStore.set('WT_CJ4_FPSYNC', 0);
 			} else {
-				WTDataStore.set('WT_CJ4_FPSYNC', 1)
+				WTDataStore.set('WT_CJ4_FPSYNC', 1);
 			}
 			B787_10_FMC_HeavyConfigurationPage.ShowPage1(fmc);
-		}
+		};
+
+		fmc.onRightInput[2] = () => {
+			if (this.useImperial()) {
+				HeavyDataStorage.set('USE_IMPERIAL', '0');
+			} else {
+				HeavyDataStorage.set('USE_IMPERIAL', '1');
+			}
+			B787_10_FMC_HeavyConfigurationPage.ShowPage1(fmc);
+		};
 	}
 
-	static isSimBriefFilled(){
+	static isSimBriefFilled() {
 		let username = HeavyDataStorage.get('SIMBRIEF_USERNAME', '');
 		let userid = HeavyDataStorage.get('SIMBRIEF_USERID', '');
 
 		return (username !== '' || userid !== '');
 	}
 
-	static isFPSyncActive(){
+	static isFPSyncActive() {
 		return (!!WTDataStore.get('WT_CJ4_FPSYNC', 0));
+	}
+
+	static useImperial() {
+		const imperial = HeavyDataStorage.get('USE_IMPERIAL', 1);
+		if(imperial == 1){
+			return true
+		}
+		return false;
 	}
 }
