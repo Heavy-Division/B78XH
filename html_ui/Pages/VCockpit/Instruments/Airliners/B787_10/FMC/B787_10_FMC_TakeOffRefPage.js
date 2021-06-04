ab19ac5eb5fb78fd8f60b32d467868bed8f51a43
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-06-04 02:18:19 +0200

    FMC -> TAKEOFF Page -> Added page 2 FMC -> TAKEOFF Page -> Page 1 changed FMC -> TAKEOFF Page -> Page 1 -> THR REDUCTION moved to page 2 FMC -> TAKEOFF Page -> Page 1 -> Added THRUST + possibility to change temperature FMC -> TAKEOFF Page -> Page 1 -> Added RUNWAY POS + possibility to change runway FMC -> TAKEOFF Page -> Page 1 -> Added GR WT FMC -> TAKEOFF Page -> Page 1 -> Added TOGW placeholder FMC -> TAKEOFF Page -> Page 1 -> Added TAKEOFF DATA REQUEST placeholder
    FMC -> TAKEOFF Page -> Page 2 -> Added WIND placeholder
    FMC -> TAKEOFF Page -> Page 2 -> Added RWY WIND placeholder
    FMC -> TAKEOFF Page -> Page 2 -> Added SLOPE/COND placeholder
    FMC -> TAKEOFF Page -> Page 2 -> Added EO ACCEL HT placeholder
    FMC -> TAKEOFF Page -> Page 2 -> Added ACCEL HT placeholder
    FMC -> TAKEOFF Page -> Page 2 -> Added LIM TOGW placeholder
    FMC -> TAKEOFF Page -> Page 2 -> Added REF OAT placeholder
    FMC -> TAKEOFF Page -> Page 2 -> THR REDUCTION moved from Page 1

--- html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC_TakeOffRefPage.js
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC_TakeOffRefPage.js
@@ -71,21 +71,33 @@ class B787_10_FMC_TakeOffRefPage {
 				}
 			}
 		};
-		let thrRedCell = '';
-		if (isFinite(fmc.thrustReductionAltitude)) {
-			thrRedCell = fmc.thrustReductionAltitude.toFixed(0);
-		} else {
-			thrRedCell = '---';
+
+
+		const selectedTemp = fmc.getThrustTakeOffTemp();
+		let selectedTempCell = '[settable]' + selectedTemp + '[/settable]';
+		selectedTempCell = selectedTempCell + '°C';
+
+		let thrustTOMode = fmc.getThrustTakeOffMode();
+		if(thrustTOMode === 0){
+			selectedTempCell += ' ' + (selectedTemp ? 'D-TO': 'TO')
+		} else if (thrustTOMode === 1){
+			selectedTempCell += ' ' + (selectedTemp ? 'D-TO 1': 'TO 1')
+		} else if (thrustTOMode === 2){
+			selectedTempCell += ' ' + (selectedTemp ? 'D-TO 2': 'TO 2')
 		}
-		thrRedCell = fmc.makeSettable(thrRedCell);
-		thrRedCell += 'FT';
-		fmc.onLeftInput[2] = () => {
+
+		fmc.onLeftInput[1] = () => {
 			let value = fmc.inOut;
 			fmc.clearUserInput();
-			if (fmc.trySetThrustReductionAccelerationAltitude(value)) {
+			if (fmc.setThrustTakeOffTemp(value)) {
 				B787_10_FMC_TakeOffRefPage.ShowPage1(fmc);
 			}
 		};
+
+
+
+
+
 		let flapsCell = '---';
 		let flapsAngle = fmc.getTakeOffFlap();
 		if (isFinite(flapsAngle) && flapsAngle >= 0) {
@@ -104,23 +116,43 @@ class B787_10_FMC_TakeOffRefPage {
 				B787_10_FMC_TakeOffRefPage.ShowPage1(fmc);
 			}
 		};
+
 		let runwayCell = '---';
 		let selectedRunway = fmc.flightPlanManager.getDepartureRunway();
 		if (selectedRunway) {
-			runwayCell = 'RW ' + Avionics.Utils.formatRunway(selectedRunway.designation);
+			runwayCell = 'RW' + Avionics.Utils.formatRunway(selectedRunway.designation);
 		}
+		runwayCell = fmc.makeSettable(runwayCell);
+
+		if (fmc.flightPlanManager.getOrigin()) {
+			fmc.onLeftInput[3] = () => {
+				let value = fmc.inOut;
+				fmc.clearUserInput();
+				fmc.setOriginRunway(value, (result) => {
+					if (result) {
+						B787_10_FMC_TakeOffRefPage.ShowPage1(fmc);
+					}
+				});
+			};
+		}
+		const useImperial = B787_10_FMC_HeavyConfigurationPage.useImperial();
+		let grWtCell = '□□□.□';
+		if (isFinite(fmc.getFuelVarsUpdatedGrossWeight(useImperial))) {
+			grWtCell = fmc.getFuelVarsUpdatedGrossWeight(useImperial).toFixed(1) + (useImperial ? ' lb' : ' kg');
+		}
+
 		fmc.setTemplate([
-			['TAKEOFF REF'],
+			['TAKEOFF REF', 1, 2],
 			['FLAPS', 'V1'],
 			[flapsCell, v1],
-			['E/O ACCEL HT', 'VR'],
-			['000FT', vR],
-			['THR REDUCTION', 'V2'],
-			[thrRedCell, v2],
-			['WIND/SLOPE', 'CG', 'TRIM'],
-			['H00/U0.0'],
-			['RW COND', 'SHIFT', 'POS'],
-			['DRY', '', runwayCell],
+			['THRUST', 'VR'],
+			[selectedTempCell, vR],
+			['CG TRIM', 'V2'],
+			['', v2],
+			['RUNWAY POS', 'TOGW', '', 'GR WT'],
+			[runwayCell + '/----', '', '', grWtCell],
+			['TAKEOFF DATA', ''],
+			['<REQUEST', '', ''],
 			['__FMCSEPARATOR'],
 			['\<INDEX', '<THRUST LIM']
 		]);
@@ -129,6 +161,71 @@ class B787_10_FMC_TakeOffRefPage {
 		};
 		fmc.onRightInput[5] = () => {
 			B787_10_FMC_ThrustLimPage.ShowPage1(fmc);
+		};
+
+		fmc.onPrevPage = () => {
+			B787_10_FMC_TakeOffRefPage.ShowPage2(fmc);
+		};
+		fmc.onNextPage = () => {
+			B787_10_FMC_TakeOffRefPage.ShowPage2(fmc);
+		};
+		fmc.updateSideButtonActiveStatus();
+	}
+
+	static ShowPage2(fmc) {
+		fmc.clearDisplay();
+		B787_10_FMC_TakeOffRefPage._timer = 0;
+		fmc.pageUpdate = () => {
+			B787_10_FMC_TakeOffRefPage._timer++;
+			if (B787_10_FMC_TakeOffRefPage._timer >= 15) {
+				B787_10_FMC_TakeOffRefPage.ShowPage2(fmc);
+			}
+		};
+
+		let thrRedCell = '';
+		if (isFinite(fmc.thrustReductionAltitude)) {
+			thrRedCell = fmc.thrustReductionAltitude.toFixed(0);
+		} else {
+			thrRedCell = '---';
+		}
+		thrRedCell = fmc.makeSettable(thrRedCell);
+		thrRedCell = 'FT' + thrRedCell;
+		fmc.onRightInput[2] = () => {
+			let value = fmc.inOut;
+			fmc.clearUserInput();
+			if (fmc.trySetThrustReductionAccelerationAltitude(value)) {
+				B787_10_FMC_TakeOffRefPage.ShowPage2(fmc);
+			}
+		};
+
+		fmc.setTemplate([
+			['TAKEOFF REF', 2, 2],
+			['', 'EO ACCEL HT'],
+			['', '1500FT'],
+			['', 'ACCEL HT'],
+			['', '1500FT'],
+			['WIND', 'THR REDUCTION'],
+			['000°/0KT', thrRedCell],
+			['RWY WIND', 'LIM TOGW'],
+			['0KTH 9KTR', ''],
+			['SLOPE/COND', 'REF OAT'],
+			['U0.0/DRY', '', ''],
+			['__FMCSEPARATOR'],
+			['\<INDEX', '<THRUST LIM']
+		]);
+
+		fmc.onLeftInput[5] = () => {
+			B787_10_FMC_InitRefIndexPage.ShowPage1(fmc);
+		};
+		fmc.onRightInput[5] = () => {
+			B787_10_FMC_ThrustLimPage.ShowPage1(fmc);
+		};
+
+		fmc.onPrevPage = () => {
+			B787_10_FMC_TakeOffRefPage.ShowPage1(fmc);
+		};
+		fmc.onNextPage = () => {
+			B787_10_FMC_TakeOffRefPage.ShowPage1(fmc);
 		};
 		fmc.updateSideButtonActiveStatus();
 	}
