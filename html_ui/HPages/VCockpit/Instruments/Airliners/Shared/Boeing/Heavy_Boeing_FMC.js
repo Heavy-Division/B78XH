fc6e9eae88a05e6c03a1c7a3789bdf61e5660edd
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-07-29 23:09:06 +0200

    advanced exec handling, route departure route arrivals

--- html_ui/HPages/VCockpit/Instruments/Airliners/Shared/Boeing/Heavy_Boeing_FMC.js
+++ html_ui/HPages/VCockpit/Instruments/Airliners/Shared/Boeing/Heavy_Boeing_FMC.js
@@ -3,16 +3,38 @@ class Heavy_Boeing_FMC extends Boeing_FMC {
 		super(...arguments);
 		this._fpHasChanged = false;
 		this._activatingDirectTo = false;
+
+		/**
+		 * Reason of this property is wrong activeRoute function and wrong using of _isRouteActivated property.
+		 * Normal behavior is that once route is activated by ACTIVATE prompt and EXEC all others modifications of route
+		 * automatically activate EXEC for direct executing and storing the changes in FMC.
+		 * When route is not activated by ACTIVATE prompt any changes do not activate EXEC and only way to activate
+		 * the EXEC is use ACTIVATE prompt
+		 *
+		 * ASOBO behavior:
+		 * _isRouteActivated is used as flag for awaiting changes for execution in a route and as EXEC illumination FLAG.
+		 *
+		 * STATES OF ROUTE:
+		 *
+		 * NOT ACTIVATED -> Route is not activated -> ACTIVATE prompt not pushed and EXECUTED, changes in route do not illuminate EXEC
+		 * ACTIVATED -> Route is activated -> ACTIVATE prompt pushed and EXECUTED, changes in route illuminate EXEC
+		 * MODIFIED -> Route is modified -> ACTIVATED and changes awaiting for execution (EXEC illuminating)
+		 *
+		 * This property holds ACTIVATED / NOT ACTIVATED state because of the misuse of _isRouteActivated in default Asobo implementation
+		 * @type {boolean}
+		 * @private
+		 */
+		this._isMainRouteActivated = false;
 	}
 
 	Init() {
 		super.Init();
-		
 		this.onExec = () => {
 			if (this.onExecPage) {
 				console.log('if this.onExecPage');
 				this.onExecPage();
 			} else {
+				console.log('else this.onExecPage');
 				this._isRouteActivated = false;
 				this.fpHasChanged = false;
 				this._activatingDirectTo = false;
@@ -63,9 +85,66 @@ class Heavy_Boeing_FMC extends Boeing_FMC {
 		if (directTo) {
 			this._activatingDirectTo = true;
 		}
-		this._isRouteActivated = true;
 		this.fpHasChanged = true;
-		SimVar.SetSimVarValue('L:FMC_EXEC_ACTIVE', 'number', 1);
+		if (this._isMainRouteActivated) {
+			this._isRouteActivated = true;
+			SimVar.SetSimVarValue('L:FMC_EXEC_ACTIVE', 'number', 1);
+		}
 		callback();
+	}
+
+	eraseRouteModifications() {
+		this.fpHasChanged = false;
+		this._activatingDirectTo = false;
+		this._isRouteActivated = false;
+		SimVar.SetSimVarValue('L:FMC_EXEC_ACTIVE', 'number', 0);
+	}
+
+	activateMainRoute() {
+		this._isMainRouteActivated = true;
+		this.activateRoute();
+	}
+
+	//function added to set departure enroute transition index
+	setDepartureEnrouteTransitionIndex(departureEnrouteTransitionIndex, callback = EmptyCallback.Boolean) {
+		this.ensureCurrentFlightPlanIsTemporary(() => {
+			this.flightPlanManager.setDepartureEnRouteTransitionIndex(departureEnrouteTransitionIndex, () => {
+				callback(true);
+			});
+		});
+	}
+
+	//function added to set arrival runway transition index
+	setArrivalRunwayTransitionIndex(arrivalRunwayTransitionIndex, callback = EmptyCallback.Boolean) {
+		this.ensureCurrentFlightPlanIsTemporary(() => {
+			this.flightPlanManager.setArrivalRunwayIndex(arrivalRunwayTransitionIndex, () => {
+				callback(true);
+			});
+		});
+	}
+
+	setArrivalAndRunwayIndex(arrivalIndex, enrouteTransitionIndex, callback = EmptyCallback.Boolean) {
+		this.ensureCurrentFlightPlanIsTemporary(() => {
+			let landingRunway = this.vfrLandingRunway;
+			if (landingRunway === undefined) {
+				landingRunway = this.flightPlanManager.getApproachRunway();
+			}
+			this.flightPlanManager.setArrivalProcIndex(arrivalIndex, () => {
+				this.flightPlanManager.setArrivalEnRouteTransitionIndex(enrouteTransitionIndex, () => {
+					if (landingRunway) {
+						const arrival = this.flightPlanManager.getArrival();
+						const arrivalRunwayIndex = arrival.runwayTransitions.findIndex(t => {
+							return t.name.indexOf(landingRunway.designation) != -1;
+						});
+						if (arrivalRunwayIndex >= -1) {
+							return this.flightPlanManager.setArrivalRunwayIndex(arrivalRunwayIndex, () => {
+								return callback(true);
+							});
+						}
+					}
+					return callback(true);
+				});
+			});
+		});
 	}
 }
