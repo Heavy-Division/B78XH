bcd858d3160cd059205da43d13c32d4a3065fb21
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-07-29 17:17:03 +0200

    Custom exec handling moved to Heavy_Boeing_FMC.js

--- html_ui/HPages/VCockpit/Instruments/Airliners/Shared/Boeing/Heavy_Boeing_FMC.js
+++ html_ui/HPages/VCockpit/Instruments/Airliners/Shared/Boeing/Heavy_Boeing_FMC.js
@@ -1,5 +1,71 @@
 class Heavy_Boeing_FMC extends Boeing_FMC {
 	constructor() {
 		super(...arguments);
+		this._fpHasChanged = false;
+		this._activatingDirectTo = false;
+	}
+
+	Init() {
+		super.Init();
+		
+		this.onExec = () => {
+			if (this.onExecPage) {
+				console.log('if this.onExecPage');
+				this.onExecPage();
+			} else {
+				this._isRouteActivated = false;
+				this.fpHasChanged = false;
+				this._activatingDirectTo = false;
+			}
+		};
+		this.onExecPage = undefined;
+		this.onExecDefault = () => {
+			if (this.getIsRouteActivated() && !this._activatingDirectTo) {
+				this.insertTemporaryFlightPlan(() => {
+					this.copyAirwaySelections();
+					this._isRouteActivated = false;
+					this._activatingDirectToExisting = false;
+					this.fpHasChanged = false;
+					SimVar.SetSimVarValue('L:FMC_EXEC_ACTIVE', 'number', 0);
+					if (this.refreshPageCallback) {
+						this.refreshPageCallback();
+					}
+				});
+			} else if (this.getIsRouteActivated() && this._activatingDirectTo) {
+				const activeIndex = this.flightPlanManager.getActiveWaypointIndex();
+				this.insertTemporaryFlightPlan(() => {
+					this.flightPlanManager.activateDirectToByIndex(activeIndex, () => {
+						this.copyAirwaySelections();
+						this._isRouteActivated = false;
+						this._activatingDirectToExisting = false;
+						this._activatingDirectTo = false;
+						this.fpHasChanged = false;
+						SimVar.SetSimVarValue('L:FMC_EXEC_ACTIVE', 'number', 0);
+						if (this.refreshPageCallback) {
+							this.refreshPageCallback();
+						}
+					});
+				});
+			} else {
+				this.fpHasChanged = false;
+				this._isRouteActivated = false;
+				SimVar.SetSimVarValue('L:FMC_EXEC_ACTIVE', 'number', 0);
+				if (this.refreshPageCallback) {
+					this._activatingDirectTo = false;
+					this.fpHasChanged = false;
+					this.refreshPageCallback();
+				}
+			}
+		};
+	}
+
+	activateRoute(directTo = false, callback = EmptyCallback.Void) {
+		if (directTo) {
+			this._activatingDirectTo = true;
+		}
+		this._isRouteActivated = true;
+		this.fpHasChanged = true;
+		SimVar.SetSimVarValue('L:FMC_EXEC_ACTIVE', 'number', 1);
+		callback();
 	}
 }
--- html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC.js
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC.js
@@ -112,56 +112,6 @@ class B787_10_FMC extends Heavy_Boeing_FMC {
 				B787_10_FMC_VNAVPage.ShowPage1(this);
 			};
 
-			this.onExec = () => {
-				if (this.onExecPage) {
-					console.log('if this.onExecPage');
-					this.onExecPage();
-				} else {
-					this._isRouteActivated = false;
-					this.fpHasChanged = false;
-					this._activatingDirectTo = false;
-				}
-			};
-			this.onExecPage = undefined;
-			this.onExecDefault = () => {
-				if (this.getIsRouteActivated() && !this._activatingDirectTo) {
-					this.insertTemporaryFlightPlan(() => {
-						this.copyAirwaySelections();
-						this._isRouteActivated = false;
-						this._activatingDirectToExisting = false;
-						this.fpHasChanged = false;
-						SimVar.SetSimVarValue('L:FMC_EXEC_ACTIVE', 'number', 0);
-						if (this.refreshPageCallback) {
-							this.refreshPageCallback();
-						}
-					});
-				} else if (this.getIsRouteActivated() && this._activatingDirectTo) {
-					const activeIndex = this.flightPlanManager.getActiveWaypointIndex();
-					this.insertTemporaryFlightPlan(() => {
-						this.flightPlanManager.activateDirectToByIndex(activeIndex, () => {
-							this.copyAirwaySelections();
-							this._isRouteActivated = false;
-							this._activatingDirectToExisting = false;
-							this._activatingDirectTo = false;
-							this.fpHasChanged = false;
-							SimVar.SetSimVarValue('L:FMC_EXEC_ACTIVE', 'number', 0);
-							if (this.refreshPageCallback) {
-								this.refreshPageCallback();
-							}
-						});
-					});
-				} else {
-					this.fpHasChanged = false;
-					this._isRouteActivated = false;
-					SimVar.SetSimVarValue('L:FMC_EXEC_ACTIVE', 'number', 0);
-					if (this.refreshPageCallback) {
-						this._activatingDirectTo = false;
-						this.fpHasChanged = false;
-						this.refreshPageCallback();
-					}
-				}
-			};
-
 			this._pointer = this.getChildById('fms-pointer');
 			this._pointer.style.zIndex = '5';
 			this._pointer.style.position = 'fixed';
