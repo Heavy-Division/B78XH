a53734b8dcd6d9388b5289710db4f3e616657b7c
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-08-21 13:20:37 +0200

    FMC added base file

---
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC_IdentPage.js
@@ -0,0 +1,36 @@
+class B787_10_FMC_IdentPage {
+	static ShowPage1(fmc) {
+		fmc.clearDisplay();
+		let model = SimVar.GetSimVarValue('ATC MODEL', 'string', 'FMC');
+		if (!model) {
+			model = 'unkn.';
+		}
+		let date = fmc.getNavDataDateRange();
+		fmc.setTemplate([
+			['IDENT'],
+			['MODEL', 'ENGINES'],
+			['787-10', 'GEnx-2B67B'],
+			['NAV DATA', 'ACTIVE'],
+			['AIRAC', date + ''],
+			['DRAG/FF'],
+			[''],
+			['OP PROGRAM', 'CO DATA'],
+			['AW-P010-0-0', 'VS1001'],
+			['OPC'],
+			['AW-C010-0-0', ''],
+			['--------------------------------------'],
+			['\<INDEX', '<POS INIT']
+		]);
+		if (fmc.urlConfig.index == 1) {
+			fmc.onLeftInput[5] = () => {
+				B787_10_FMC_InitRefIndexPage.ShowPage1(fmc);
+			};
+			fmc.onRightInput[5] = () => {
+				B787_10_FMC_PosInitPage.ShowPage1(fmc);
+			};
+		}
+		fmc.updateSideButtonActiveStatus();
+	}
+}
+
+//# sourceMappingURL=B787_10_FMC_IdentPage.js.map
