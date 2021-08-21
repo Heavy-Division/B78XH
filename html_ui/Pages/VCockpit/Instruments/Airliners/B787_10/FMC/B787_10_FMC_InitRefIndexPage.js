a2dd73fefaa47a3a21e6269f1d947426c7210253
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-08-21 13:37:05 +0200

    FMC base INIT REF page added

---
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC_InitRefIndexPage.js
@@ -0,0 +1,44 @@
+class B787_10_FMC_InitRefIndexPage {
+	static ShowPage1(fmc) {
+		fmc.clearDisplay();
+		fmc.setTemplate([
+			['INIT/REF INDEX'],
+			[''],
+			['\<IDENT[color]blue', '<NAV DATA[color]red'],
+			[''],
+			['\<POS'],
+			[''],
+			['\<PERF[color]yellow'],
+			[''],
+			['\<THRUST LIM'],
+			[''],
+			['\<TAKEOFF[color]green'],
+			[''],
+			['\<APPROACH', '<MAINT']
+		]);
+		fmc.onLeftInput[0] = () => {
+			B787_10_FMC_IdentPage.ShowPage1(fmc);
+		};
+		fmc.onLeftInput[1] = () => {
+			B787_10_FMC_PosInitPage.ShowPage1(fmc);
+		};
+		fmc.onLeftInput[2] = () => {
+			B787_10_FMC_PerfInitPage.ShowPage1(fmc);
+		};
+		fmc.onLeftInput[3] = () => {
+			B787_10_FMC_ThrustLimPage.ShowPage1(fmc);
+		};
+		fmc.onLeftInput[4] = () => {
+			B787_10_FMC_TakeOffRefPage.ShowPage1(fmc);
+		};
+		fmc.onLeftInput[5] = () => {
+			B787_10_FMC_ApproachPage.ShowPage1(fmc);
+		};
+		fmc.onRightInput[5] = () => {
+			B787_10_FMC_MaintPage.ShowPage1(fmc);
+		};
+		fmc.updateSideButtonActiveStatus();
+	}
+}
+
+//# sourceMappingURL=B787_10_FMC_InitRefIndexPage.js.map
