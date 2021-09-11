a5507a44cff5d0b0437d866093a602402a541ef2
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-09-11 23:58:21 +0200

    HeavyEventDispatcher updated

--- html_ui/Heavy/Utils/HeavyEventDispatcher.js
+++ html_ui/Heavy/Utils/HeavyEventDispatcher.js
@@ -10,11 +10,11 @@ let HeavyEventDispatcher;
 	 * @param event
 	 * @param target
 	 */
-	HeavyEventDispatcher.trigger = function (event, target) {
-		/**
-		 * TODO: implement
-		 */
-		//console.log(HeavyEventDispatcher.target[target] + HeavyEventDispatcher.event[target]);
+	HeavyEventDispatcher.trigger = function (event, target, debug = false) {
+		if(debug){
+			console.log(HeavyEventDispatcher.target[target] + HeavyEventDispatcher.event[event]);
+		}
+		SimVar.SetSimVarValue(HeavyEventDispatcher.target[target] + HeavyEventDispatcher.event[event], 'Number', 0);
 	};
 
 	/**
@@ -22,10 +22,12 @@ let HeavyEventDispatcher;
 	 */
 	let target;
 	(function (target) {
-		target[target['MFD_1'] = 0] = 'H:AS01B_MFD_1';
-		target[target['MFD_2'] = 1] = 'H:AS01B_MFD_2';
-		target[target['FMC_1'] = 2] = 'H:AS01B_FMC_1';
-		target[target['FMC_2'] = 3] = 'H:AS01B_FMC_2';
+		target[target['MFD_1'] = 0] = 'H:AS01B_MFD_1_';
+		target[target['MFD_2'] = 1] = 'H:AS01B_MFD_2_';
+		target[target['FMC_1'] = 2] = 'H:AS01B_FMC_1_';
+		target[target['FMC_2'] = 3] = 'H:AS01B_FMC_2_';
+		target[target['PFD'] = 4] = 'H:AS01B_PFD_';
+		target[target['GLOBAL'] = 100] = '';
 	})(target || (target = {}));
 
 	HeavyEventDispatcher.target = target;
@@ -39,26 +41,51 @@ let HeavyEventDispatcher;
 		 * Internal events
 		 * @type {string}
 		 */
-		event[event['Cursor_INC'] = 0] = 'Cursor_INC';
-		event[event['Cursor_DEC'] = 1] = 'Cursor_DEC';
-		event[event['Cursor_Press'] = 2] = 'Cursor_Press';
-		event[event['AUTOPILOT_CTR'] = 3] = 'AUTOPILOT_CTR';
-		event[event['AP_HEADING_HOLD'] = 4] = 'AP_HEADING_HOLD';
-		event[event['AP_ALT_HOLD'] = 5] = 'AP_ALT_HOLD';
-		event[event['AP_FLCH'] = 6] = 'AP_FLCH';
-		event[event['AP_VSPEED'] = 7] = 'AP_VSPEED';
-		event[event['AP_LNAV'] = 8] = 'AP_LNAV';
-		event[event['AP_VNAV'] = 9] = 'AP_VNAV';
-		event[event['AP_SPD'] = 10] = 'AP_SPD';
-		event[event['AP_SPEED_INTERVENTION'] = 11] = 'AP_SPEED_INTERVENTION';
-		event[event['AP_HEADING_SEL'] = 12] = 'AP_HEADING_SEL';
-		event[event['AP_ALT_INTERVENTION'] = 13] = 'AP_ALT_INTERVENTION';
-		event[event['THROTTLE_TO_GA'] = 14] = 'THROTTLE_TO_GA';
+		event[event['AP_HEADING_HOLD'] = 0] = 'AP_HEADING_HOLD';
+		event[event['AP_ALT_HOLD'] = 1] = 'AP_ALT_HOLD';
+		event[event['AP_FLCH'] = 2] = 'AP_FLCH';
+		event[event['AP_VSPEED'] = 3] = 'AP_VSPEED';
+		event[event['AP_LNAV'] = 4] = 'AP_LNAV';
+		event[event['AP_VNAV'] = 5] = 'AP_VNAV';
+		event[event['AP_SPD'] = 6] = 'AP_SPD';
+		event[event['AP_SPEED_INTERVENTION'] = 7] = 'AP_SPEED_INTERVENTION';
+		event[event['AP_HEADING_SEL'] = 8] = 'AP_HEADING_SEL';
+		event[event['AP_ALT_INTERVENTION'] = 9] = 'AP_ALT_INTERVENTION';
+		event[event['THROTTLE_TO_GA'] = 10] = 'THROTTLE_TO_GA';
+
+		/**
+		 * ND Events
+		 * @type {string}
+		 */
+		event[event['Range_INC'] = 100] = 'Range_INC';
+		event[event['Range_DEC'] = 101] = 'Range_DEC';
+		event[event['AUTOPILOT_CTR'] = 103] = 'AUTOPILOT_CTR';
+		event[event['Cursor_INC'] = 104] = 'Cursor_INC';
+		event[event['Cursor_DEC'] = 105] = 'Cursor_DEC';
+		event[event['Cursor_Press'] = 106] = 'Cursor_Press';
+		event[event['DSP_SYS'] = 107] = 'SYS';
+		event[event['DSP_CDU'] = 108] = 'CDU';
+		event[event['DSP_INFO'] = 109] = 'INFO';
+		event[event['DSP_CHKL'] = 110] = 'CHKL';
+		event[event['DSP_COMM'] = 111] = 'COMM';
+		event[event['DSP_ND'] = 112] = 'ND';
+		event[event['DSP_EICAS'] = 113] = 'EICAS';
+		event[event['DSP_ENG'] = 114] = 'ENG';
+
+		/**
+		 * PFD Events
+		 * @type {string}
+		 */
+		event[event['Mins_INC'] = 200] = 'Mins_INC';
+		event[event['Mins_DEC'] = 201] = 'Mins_DEC';
+		event[event['Mins_RST'] = 202] = 'Mins_Press'; /* B:AIRLINER_Mins_Button_Push global event can be also use */
+
+
 		/**
 		 * Heavy events
 		 * @type {string}
 		 */
-		event[event['B78XH_TAKEOFF_MODES_UPDATED'] = 20] = 'B78XH_TAKEOFF_MODES_UPDATED';
+		event[event['B78XH_TAKEOFF_MODES_UPDATED'] = 1000] = 'B78XH_TAKEOFF_MODES_UPDATED';
 	})(event || (event = {}));
 
 	HeavyEventDispatcher.event = event;
