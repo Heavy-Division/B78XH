b706b25ff3afd91c89cae5dffe71858ce9c86496
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-10-06 04:28:57 +0200

    HDSDK updated

--- html_ui/Heavy/libs/hdsdk.js
+++ html_ui/Heavy/libs/hdsdk.js
@@ -233,6 +233,127 @@
         }
     }
 
+    (function (HeavyInput) {
+        class Converters {
+            static inputToAltitude(input) {
+                let inputCheck = input.split('FL');
+                if (inputCheck.length === 1) {
+                    return isFinite(Number(inputCheck[0])) ? Number(inputCheck[0]) : false;
+                }
+                else {
+                    if (inputCheck[0] === '' && isFinite(Number(inputCheck[1]))) {
+                        return Number(inputCheck[1]) * 100;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+            static convertAltitudeDescriptionLettersToIndexes(input) {
+                switch (input) {
+                    case '':
+                        return 1;
+                    case 'A':
+                        return 2;
+                    case 'B':
+                        return 3;
+                    case 'AB':
+                        return 4;
+                    default:
+                        return 0;
+                }
+            }
+            static waypointConstraints(input, convertToFeet = true, convertAltitudeDescriptionLettersToIndexes = true) {
+                let inputCheck = input;
+                let inputArray;
+                let output = {
+                    speed: -1,
+                    altitudes: []
+                };
+                let stringAltitudes = [];
+                let speed;
+                let altitudes;
+                if (inputCheck.indexOf('/') !== -1) {
+                    inputArray = inputCheck.split('/');
+                    if (inputArray.length !== 2) {
+                        return false;
+                    }
+                    else {
+                        speed = inputArray[0];
+                        altitudes = inputArray[1];
+                    }
+                }
+                else {
+                    altitudes = inputCheck;
+                }
+                if (speed) {
+                    if (Validators.speedRange(speed)) {
+                        output.speed = Math.round(parseInt(speed));
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                if (altitudes) {
+                    let match = altitudes.match(/^([0-9]{4}|[0-5][0-9]{4}|FL[0-5][0-9]{2}|[0-5][0-9]{2})([AB]?)$/);
+                    if (!match) {
+                        match = altitudes.match(/^([0-9]{4}|[0-5][0-9]{4}|FL[0-5][0-9]{2}|[0-5][0-9]{2})(A)([0-9]{4}|[0-5][0-9]{4}|FL[0-5][0-9]{2}|[0-5][0-9]{2})(B)$/);
+                    }
+                    if (match) {
+                        match.forEach((value) => {
+                            stringAltitudes.push(String(value));
+                        });
+                    }
+                }
+                if (stringAltitudes) {
+                    if (convertToFeet) {
+                        for (let i = 1; i < stringAltitudes.length - 1; i++) {
+                            if (stringAltitudes[i].indexOf('FL') !== -1) {
+                                stringAltitudes[i] = stringAltitudes[i].replace('FL', '');
+                                output.altitudes[i] = parseInt(stringAltitudes[i]) * 100;
+                            }
+                            else {
+                                output.altitudes[i] = parseInt(stringAltitudes[i]);
+                            }
+                        }
+                    }
+                    if (convertAltitudeDescriptionLettersToIndexes) {
+                        if (stringAltitudes[2] || stringAltitudes[2] === '') {
+                            output.altitudes[2] = this.convertAltitudeDescriptionLettersToIndexes(stringAltitudes[2]);
+                        }
+                        if (stringAltitudes[5]) {
+                            output.altitudes[5] = this.convertAltitudeDescriptionLettersToIndexes(stringAltitudes[5]);
+                        }
+                    }
+                }
+                return (output.speed !== -1 || output.altitudes ? output : false);
+            }
+        }
+        HeavyInput.Converters = Converters;
+        class Validators {
+            static speedRange(input, min = 100, max = 399) {
+                const inputCheck = Number(input);
+                return isFinite(inputCheck) && inputCheck >= min && inputCheck <= max;
+            }
+            static altitudeRange(input, min = 0, max = Infinity) {
+                const inputCheck = Number(input);
+                return isFinite(inputCheck) && inputCheck >= min && inputCheck <= max;
+            }
+            static speedRangeWithAltitude(input) {
+                const inputCheck = input.split('/');
+                return inputCheck.length === 2 && inputCheck[0] !== undefined && inputCheck[1] !== undefined && this.speedRange(inputCheck[0]) && this.altitudeRange(inputCheck[1]);
+            }
+            static speedRestriction(input, cruiseAltitude) {
+                if (!this.speedRangeWithAltitude(input) || !this.altitudeRange(String(cruiseAltitude))) {
+                    return false;
+                }
+                let inputCheck = input.split('/');
+                return inputCheck.length === 2 && inputCheck[0] !== undefined && inputCheck[1] !== undefined && this.speedRange(inputCheck[0]) && this.altitudeRange(inputCheck[1], 0, Number(cruiseAltitude));
+            }
+        }
+        HeavyInput.Validators = Validators;
+    })(exports.HeavyInput || (exports.HeavyInput = {}));
+
     exports.SimBrief = SimBrief;
     exports.SimBriefCredentials = SimBriefCredentials;
     exports.SimBriefApi = SimBriefApi;
