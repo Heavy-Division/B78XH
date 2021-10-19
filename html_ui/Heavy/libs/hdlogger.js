33e0d591f1a44afd79762bd6ca7a294147425405
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-10-19 23:41:48 +0200

    SU6 ORIGIN patch - It is possible to set ORIGIN. SimBrief import is fixed.

---
+++ html_ui/Heavy/libs/hdlogger.js
@@ -0,0 +1,61 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (global = global || self, factory(global.window = global.window || {}));
+}(this, function (exports) { 'use strict';
+
+    var Level;
+    (function (Level) {
+        Level[Level["none"] = 0] = "none";
+        Level[Level["debug"] = 10000] = "debug";
+        Level[Level["info"] = 20000] = "info";
+        Level[Level["warning"] = 30000] = "warning";
+        Level[Level["error"] = 40000] = "error";
+        Level[Level["fatal"] = 50000] = "fatal";
+    })(Level || (Level = {}));
+
+    class HDLogger {
+        static log(message, level = Level.none) {
+            if (this.handlers && this.handlers.length > 0) {
+                for (let handler of this.handlers) {
+                    handler.log(message, level);
+                }
+            }
+        }
+        static addHandler(handler) {
+            this.handlers.push(handler);
+        }
+    }
+    HDLogger.handlers = [];
+
+    class ConsoleHandler {
+        log(message, level = Level.none) {
+            switch (level) {
+                case Level.none:
+                    console.log(message);
+                    break;
+                case Level.debug:
+                    console.info(Level[level] + ': ' + message);
+                    break;
+                case Level.info:
+                    console.info(Level[level] + ': ' + message);
+                    break;
+                case Level.warning:
+                    console.warn(Level[level] + ': ' + message);
+                    break;
+                case Level.error:
+                    console.warn(Level[level] + ': ' + message);
+                    break;
+                case Level.fatal:
+                    console.warn(Level[level] + ': ' + message);
+                    break;
+            }
+        }
+    }
+
+    exports.HDLogger = HDLogger;
+    exports.ConsoleHandler = ConsoleHandler;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
--- html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC.js
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC.js
@@ -1,5 +1,25 @@
 class B787_10_FMC extends Heavy_Boeing_FMC {
 
+
+	/**
+	 * SU6 ORIGIN compatibility patch.
+	 * TODO: Should be moved to Heavy_Boeing_FMC/Boeing_FMC
+	 * @param newRouteOrigin
+	 * @param callback
+	 */
+	updateRouteOrigin(newRouteOrigin, callback = EmptyCallback.Boolean) {
+		this.dataManager.GetAirportByIdent(newRouteOrigin).then(airport => {
+			if (!airport) {
+				this.showErrorMessage('NOT IN DATABASE');
+				return callback(false);
+			}
+			this.flightPlanManager.setOrigin(airport.icao, () => {
+				this.tmpOrigin = airport.ident;
+				callback(true);
+			});
+		});
+	}
+
 	_updateAlertingMessages() {
 		if (this.messageManager.numberOfMessages > 0) {
 			let messageBoxTitle = document.body.querySelector('.fms-message-title');
@@ -494,22 +514,22 @@ class B787_10_FMC extends Heavy_Boeing_FMC {
 			let m = SimVar.GetGlobalVarValue('ZULU MONTH OF YEAR', 'number');
 			let d = SimVar.GetGlobalVarValue('ZULU DAY OF MONTH', 'number');
 			let date = new Date();
-			date.setUTCFullYear(y, m - 1, d)
+			date.setUTCFullYear(y, m - 1, d);
 			date.setUTCHours(23, 59, 59);
 
 			let navDataDateRange = this.getNavDataDateRange();
-			let navDataDateArray = navDataDateRange.substring(navDataDateRange.length - 8).split('/')
-			let m1 = B787_10_FMC._MonthOfYear.findIndex(function(element){
+			let navDataDateArray = navDataDateRange.substring(navDataDateRange.length - 8).split('/');
+			let m1 = B787_10_FMC._MonthOfYear.findIndex(function (element) {
 				return element === navDataDateArray[0].substring(0, 3);
-			})
+			});
 			let d1 = navDataDateArray[0].substring(navDataDateArray[0].length - 2);
-			let y1  = '20' + navDataDateArray[1]
+			let y1 = '20' + navDataDateArray[1];
 
 			let date1 = new Date();
-			date1.setUTCFullYear(parseInt(y1), m1 + 1, parseInt(d1))
+			date1.setUTCFullYear(parseInt(y1), m1 + 1, parseInt(d1));
 			date1.setUTCHours(23, 59, 59);
-			if(date1.getTime() < date.getTime()){
-				this.messageManager.showMessage('NAV DATA OUT OF DATE', 'END DATE OF THE ACTIVE <br> DATA BASE HAS PASSED <br> SELECT NEW CYCLE <br> ON IDENT PAGE')
+			if (date1.getTime() < date.getTime()) {
+				this.messageManager.showMessage('NAV DATA OUT OF DATE', 'END DATE OF THE ACTIVE <br> DATA BASE HAS PASSED <br> SELECT NEW CYCLE <br> ON IDENT PAGE');
 			}
 		}
 
