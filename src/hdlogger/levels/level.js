bc50e127ddff2c985fc24ab49bb8ac65d672e1aa
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-11-10 07:59:18 +0100

    removed js from source

--- src/hdlogger/levels/level.js
+++
@@ -1,9 +0,0 @@
-export var Level;
-(function (Level) {
-    Level[Level["none"] = 0] = "none";
-    Level[Level["debug"] = 10000] = "debug";
-    Level[Level["info"] = 20000] = "info";
-    Level[Level["warning"] = 30000] = "warning";
-    Level[Level["error"] = 40000] = "error";
-    Level[Level["fatal"] = 50000] = "fatal";
-})(Level || (Level = {}));
