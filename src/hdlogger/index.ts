19a887eca1f2e19dccb0ff210a36bc41f6c7085d
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-11-10 08:01:47 +0100

    added socket io handler

--- src/hdlogger/index.ts
+++ src/hdlogger/index.ts
@@ -1,2 +1,3 @@
-export * from './logger'
-export * from './handlers/ConsoleHandler'
+export * from './logger';
+export * from './handlers/ConsoleHandler';
+export * from './handlers/SocketIOHandler';
