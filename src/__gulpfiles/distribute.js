9ef76e31f0b6c1425bcbf6e349d9068eeee1a0f1
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-11-10 16:57:07 +0100

    fixed paths

--- src/__gulpfiles/distribute.js
+++ src/__gulpfiles/distribute.js
@@ -2,15 +2,16 @@ const gulp = require('gulp');
 const {parallel} = require('gulp');
 const ts = require('gulp-typescript');
 const merge = require('merge2');
+const rename = require('gulp-rename');
 
 const projects = {
 	b78xh: ts.createProject('tsconfig.json')
 };
 
 const paths = {
-	sdk: ['dist/hdsdk.js', '../html_ui/Heavy/libs'],
-	logger: ['dist/hdlogger.js', '../html_ui/Heavy/libs'],
-	fmc: ['dist/hdfmc.js', '../html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC']
+	sdk: ['dist/hdsdk/hdsdk.js', '../html_ui/Heavy/libs'],
+	logger: ['dist/hdlogger/hdlogger.js', '../html_ui/Heavy/libs'],
+	fmc: ['dist/hdfmc/hdfmc.js', '../html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC']
 };
 
 
