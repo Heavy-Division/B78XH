2706b58dae7aa682c886e8718ac8fd595d65e303
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-12-08 19:16:38 +0100

    gulpfiles updated

--- src/__gulpfiles/monitor.js
+++ src/__gulpfiles/monitor.js
@@ -7,7 +7,7 @@ const distributeTasks = require('./distribute');
 
 function monitorSource() {
 	log('Monitoring build folder.\n', TerminalColors.blue);
-	watch(['hdsdk/**/*.ts', 'hdfmc/**/*.ts', 'hdlogger/**/*.ts'], {ignoreInitial: false}, series(buildTasks.buildB78XH, rollupTasks.rollupHDSDK, rollupTasks.rollupHDLogger, rollupTasks.rollupHDFMC, cleanTasks.cleanBuild, distributeTasks.distribute)).on('change', function (path, stats) {
+	watch(['hdsdk/**/*.ts', 'hdfmc/**/*.ts', 'hdlogger/**/*.ts', 'hdautopilot/**/*.ts'], {ignoreInitial: false}, series(buildTasks.buildB78XH, rollupTasks.rollupHDSDK, rollupTasks.rollupHDLogger, rollupTasks.rollupHDFMC, cleanTasks.cleanBuild, distributeTasks.distribute)).on('change', function (path, stats) {
 		log('Source files were changed. Starting build process...', TerminalColors.red);
 	});
 }
